b2f4a50e4072feb253747b304bdaee8adcc03b73
cpjuanluismartell-lgtm <cpjuanluismartell@gmail.com>
2026-01-27 23:26:33 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig(({ mode }) => {
         host: '0.0.0.0',
       },
       plugins: [react()],
+          base: '/portapapeles-inteligente/', // ← Nombre de tu repositorio
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
